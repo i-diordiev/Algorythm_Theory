ca037c5533c58334e1affafa986a7e592d70561f
i-diordiev <i.diordiev-2027@kpi.ua>
2021-02-15 12:49:14 +0200

    editet lab1

--- improved_bubble_sort.py
+++ improved_bubble_sort.py
@@ -3,9 +3,11 @@ import random
 array = [random.randint(0, 100) for i in range(10)]
 print("Start array is:", array)
 
+ops = 0
 for i in array:
     isSorted = True
     for j in range(len(array) - 1):
+        ops += 1
         if array[j] > array[j+1]:
             temp = array[j]
             array[j] = array[j + 1]
@@ -15,3 +17,4 @@ for i in array:
         break
 
 print("Final array is:", array)
+print("Number of operations:", ops)
