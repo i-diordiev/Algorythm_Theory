dfce2760900e6877ea703b5d625efd2db364cda5
i-diordiev <i.diordiev-2027@kpi.ua>
2021-04-11 19:36:30 +0300

    test

--- test.py
+++ test.py
@@ -1,41 +1,28 @@
-u_r1 = [8.9, 10.3, 12.1, 14.6, 15.6, 15.4, 13.4, 11.2, 9.2]
-u_r2 = [10.2, 12.8, 17.2, 25.6, 33.5, 26.6, 18.2, 13.2, 9.8]
-u_r3 = [10.3, 13, 18.7, 30, 46.9, 33.7, 21, 15.6, 11.4]
-
-print("\n\nFOR R1:")
-for el in u_r1:
-    print(el/5)
-
-print("\n\nFOR R2:")
-for el in u_r2:
-    print(el/5)
-
-print("\n\nFOR R3:")
-for el in u_r3:
-    print(el/5)
+import math
 
 
-name = input("Enter your file: ") # - rjvvtyn
-file = open(name,'r')
-matrix = [list(map(int, row.split())) for row in file.readlines()]
-for i in range(len(matrix)):
-    for j in range(len(matrix[i])):
-        print(matrix[i][j], end=' ')
-    print()
-x = 6
-file.close()
-handle = open("is03_konyushenko_08_output.txt","w")
-handle.write(str(x)+'\n')
-for i in range(1,len(matrix)):
-    counter=0
-    for el_6 in range(1,len(matrix[i])-1):
-        for j in range(1,len(matrix[i])-1):
-            if(matrix[6][el_6]<matrix[i][j]):
-                counter+=1
-    handle.write(str(i)+' '+str(counter) + '\n')
+def factorial(n):
+    factor = 1
+    for k in range(1, n + 1):
+        factor = factor * k
+    return factor
 
-handle = open("is03_konyushenko_08_output.txt","r")
-data = handle.read()
-print("is03_konyushenko_08_output.txt:")
-print(data)
-handle.close()
+
+def ex(n):
+    k = 1
+    e_add = 1
+    e_x = 1
+    while math.fabs(e_add) >= eps:
+        e_add = (-1 ** k) * ((n ** k) / factorial(k))
+        e_x += e_add
+        k += 1
+    return e_x
+
+
+x = int(input("Type x: "))
+eps = float(input("Type eps: "))
+if 0 <= x <= 2:
+    y = ex(x) + ex(2 * x)
+elif x > 2:
+    y = (1 / ex(x + 5)) - ex(x + 2)
+print("y =", y)
